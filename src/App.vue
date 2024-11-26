881938bb1345281428734c1484edd846f4faa872
Mamun0707 <abdulalmamun637@gmail.com>
2024-11-26 13:23:56 +0600

    first commit vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    
+
+      <div class="js-animsition animsition" id="site-wrap" data-animsition-in-class="fade-in" data-animsition-out-class="fade-out">
+
+
+        <Header></Header>
+        <!-- load all other page/component -->
+        <router-view />
+
+
+        <Footer></Footer>
+      
+      </div>
+
+
+  </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Include/Header.vue'
+import Footer from './components/Include/Footer.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header, Footer
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+

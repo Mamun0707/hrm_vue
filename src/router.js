91443c6edcfba38a24e2590a3c60a366c787fa6f
Mamun0707 <abdulalmamun637@gmail.com>
2024-11-30 12:10:26 +0600

    edit

--- src/router.js
+++ src/router.js
@@ -1,29 +1,29 @@
 import { createWebHistory, createRouter } from "vue-router";
 
-const routes =  [
-  {
-    path: "/",
-    alias: "/home",
-    name: "home",
-    component: () => import("./components/Home")
-  },
-  {
-    path: "/Career",
-    alias: "/Career",
-    name: "Career",
-    component: () => import("./components/Career")
-  },
-  {
-    path: "/Blog",
-    alias: "/Blog",
-    name: "Blog",
-    component: () => import("./components/Blog")
-  }
-];
+  const routes =  [
+    {
+      path: "/",
+      alias: "/home",
+      name: "home",
+      component: () => import("./components/Home")
+    },
+    {
+      path: "/Career",
+      alias: "/Career",
+      name: "Career",
+      component: () => import("./components/Career")
+    },
+    {
+      path: "/Blog",
+      alias: "/Blog",
+      name: "Blog",
+      component: () => import("./components/Blog")
+    }
+  ];
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
+  const router = createRouter({
+    history: createWebHistory(),
+    routes,
+  });
 
-export default router;
+  export default router;

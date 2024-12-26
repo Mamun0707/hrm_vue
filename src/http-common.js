262ad2033ab7707907a27e266c4179a3e49268b1
Mamun0707 <abdulalmamun637@gmail.com>
2024-12-26 13:06:44 +0600

    base url change

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,17 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://weeblecode.com/hrm/public/api",
   headers: {
     "Content-type": "multipart/form-data"
   }
 });
+
+// import axios from "axios";
+
+// export default axios.create({
+//   baseURL: "http://127.0.0.1:8000/api",
+//   headers: {
+//     "Content-type": "multipart/form-data"
+//   }
+// });
